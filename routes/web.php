2447246b0513d62c777f0fab13d69d73cba98fb0
HONG-WEI\owner <tim5556618@gmail.com>
2017-10-17 01:34:29 +0800

    增加任務

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,22 @@ Route::get('/', function () {
 * 增加新的任務
 */
 Route::post('/task', function (Request $request) {
-//
+$validator = Validator::make($request->all(), [
+'name' => 'required|max:255',  //驗證規則
+]);
+
+if ($validator->fails()) {   //驗證失敗處理
+return redirect('/')
+->withInput()
+->withErrors($validator);
+}
+
+// 建立該任務...
+$task = new Task;
+$task->name = $request->name;
+$task->save();
+
+return redirect('/');
 });
 
 /**
