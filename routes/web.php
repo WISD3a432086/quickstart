13ad95b013e1ffbc14131dbb5ffb022907e43810
HONG-WEI\owner <tim5556618@gmail.com>
2017-10-17 01:15:33 +0800

    建置路由

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,21 @@
-<?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+/**
+* 顯示所有任務
 */
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('tasks');
+//
+});
+
+/**
+* 增加新的任務
+*/
+Route::post('/task', function (Request $request) {
+//
+});
+
+/**
+* 刪除任務
+*/
+Route::delete('/task/{task}', function (Task $task) {
+//
 });
